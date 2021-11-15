e96b86c3b21754dec5837319b31bb74501b8f60d
Pavan Rathod <edroculllen13@gmail.com>
2021-11-15 19:54:39 +0530

    my console

--- demo.js
+++ demo.js
@@ -1,1 +1,4 @@
-console.log("demo.js")
+console.log("demo.js");
+console.log(
+  "okay i merged the branch the commnad is i was in development-pavan branch then i ran this cammand git merge development"
+);
