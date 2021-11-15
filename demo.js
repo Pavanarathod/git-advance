b0788bc787e07b5664d975fe7808f9c932fa296c
Pavan Rathod <edroculllen13@gmail.com>
2021-11-15 19:58:27 +0530

    all four commits

--- demo.js
+++ demo.js
@@ -2,3 +2,5 @@ console.log("demo.js");
 console.log(
   "okay i merged the branch the commnad is i was in development-pavan branch then i ran this cammand git merge development"
 );
+
+console.log("okay everythig is fine");
